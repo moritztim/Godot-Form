16982eed37bb9e37d018f18ff6fde2762712a70d
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2023-10-23 15:23:38 +0200

    improve html

--- nodes/Protocols/MailProtocol.gd
+++ nodes/Protocols/MailProtocol.gd
@@ -7,6 +7,7 @@ enum BodyFormat {
 	JSON
 }
 @export var body_format: BodyFormat = BodyFormat.HTML
+@export_file var css := "../../styles/default.css"
 
 @export_group("Metadata")
 @export var from_name: String
@@ -20,13 +21,16 @@ func submit(fields: Dictionary) -> int:
 ## Generates an HTML string from a dictionary of fields
 func generate_body(fields: Dictionary) -> String:
 	var body := ""
+	var style := ""
 	var suffix := ""
 	var format_line := "{key}: {value}\n"
 
 	if body_format == BodyFormat.HTML:
-		body = "<html><body>"
-		format_line = "<p><b> {key} </b>: {value} </p>"
-		suffix = "</body></html>"
+		body = "<html><body><form>"
+		if FileAccess.file_exists(css):
+			style = "<style>" + FileAccess.get_file_as_string(css) + "</style>"
+		format_line = "<label for=\"{key}\">{key}</label><input disabled name=\"{key}\" type=\"{type}\" value=\"{value}\" {checked}><br>"			
+		suffix = "</form></body></html>"
 	elif body_format == BodyFormat.JSON:
 		body = "{"
 		format_line = "\"{key}\": {value},"
@@ -39,8 +43,23 @@ func generate_body(fields: Dictionary) -> String:
 				value = "\"" + value + "\""
 			if field == fields.keys().back():
 				format_line.replace(",", "")
-		body += format_line.format({"key": field, "value": value})
-	return body + suffix
+		
+		var checked : = ""
+		if fields[field]:
+			checked = "checked"
+		
+		body += format_line.format({"key": field, "value": value, "type": type_to_string(typeof(super.get_value(fields[field]))), "checked": checked})
+	return body + style + suffix
+
+static func type_to_string(type: int) -> String:
+	return {
+		TYPE_STRING: "text",
+		TYPE_BOOL: "checkbox",
+		TYPE_INT: "number",
+		TYPE_FLOAT: "number",
+		TYPE_ARRAY: "text",
+		TYPE_NIL: "text"
+	}[type]
 
 func get_value(subject: Node) -> String:
 	var value = super.get_value(subject)
