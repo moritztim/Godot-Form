3b66296d7079a3aa2970dad0e2d75615f41370c4
Moritz Tim W. <90388353+moritz-t-w@users.noreply.github.com>
2023-11-03 18:01:48 +0100

    doc and readability

--- addons/form/nodes/FormContainer.gd
+++ addons/form/nodes/FormContainer.gd
@@ -1,8 +1,11 @@
 @tool
+## Manages form submission and is intended to contain the form elements.
 class_name FormContainer extends Container
 
 @export_group("Submission")
+## Calls the submit() function when pressed.
 @export var submit_button: Submit
+## Handles the submission of the form.
 @export var protocol:Protocol
 
 
@@ -10,23 +13,45 @@ func _ready():
 	if submit_button != null:
 		submit_button.pressed.connect(submit)
 
+## Submits the form data to the protocol.
 func submit():
 	protocol.submit(generate_fields_dict())
 
-func generate_fields_dict(subject: Node = self) -> Dictionary:
+## Generates a dictionary of the form data.
+func generate_fields_dict(
+	## The node to generate the dictionary from.
+	## This is mainly used for recursion.
+	subject: Node = self
+) -> Dictionary:
 	var fields := {}
 	var labeled_inputs := []
+
 	for child in subject.get_children():
+		# If the child is a label with an associated input, add it to the dictionary.
 		if child is FormLabel && child.input != null:
 			fields[child.text] = child.input
 			labeled_inputs.append(child.input)
+		# Else if the child serves as a container, recursively add its children.
 		elif child.get_child_count() > 0:
+			# Add the child's children to the dictionary as they are returned from the recursive call.
 			fields.merge(generate_fields_dict(child))
+	
+	# Before we checked only inputs that have labels, so this adds the remaining ones.
 	for child in subject.get_children():
+		# If it's an input and it hasn't been added yet, add it.
 		if is_input(child) && ! labeled_inputs.has(child):
 			fields[child.name] = child
 	return fields
 
+## Returns whether the given node is an input.
+## Inputs are
+## - buttons except MenuButton
+## - LineEdit
+## - TextEdit
+## - ItemList
+## - Slider
+## - SpinBox
+## - GraphEdit
 static func is_input(subject: Node):
 	return (
 		# subject is input button
