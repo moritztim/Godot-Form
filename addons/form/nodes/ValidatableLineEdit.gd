70cbfa4cbba90af2b70afdbf275f8dd5741bf04b
Moritz Tim W. <90388353+moritz-t-w@users.noreply.github.com>
2023-11-06 18:12:25 +0100

    doc

--- addons/form/nodes/ValidatableLineEdit.gd
+++ addons/form/nodes/ValidatableLineEdit.gd
@@ -1,7 +1,9 @@
+## LineEdit with a validator.
 class_name VtableLineEdit extends LineEdit
 
 @export var validator: Validator
 
+## Assigns the style from the validator if it exists.
 func _init():
 	if validator:
 		validator.style_valid = get_theme_stylebox("normal")
--- addons/form/nodes/ValidatableTextEdit.gd
+++ addons/form/nodes/ValidatableTextEdit.gd
@@ -1,7 +1,9 @@
+## TextEdit with a validator.
 class_name ValidatableTextEdit extends TextEdit
 
 @export var validator: Validator
 
+## Assigns the style from the validator if it exists.
 func _init():
 	if validator:
 		validator.style_valid = get_theme_stylebox("normal")
