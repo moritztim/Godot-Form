13b983f02ef35b2716b2dae7bb5df6676bb9a169
Moritz Tim W. <90388353+moritz-t-w@users.noreply.github.com>
2023-11-06 17:38:01 +0100

    doc

--- addons/form/nodes/Protocol.gd
+++ addons/form/nodes/Protocol.gd
@@ -1,10 +1,23 @@
+## Handles form submission and response.
 class_name Protocol extends Resource
 
-## Submits form data and returns HTTP status code of the response
+## Submits form data and returns HTTP status code of the response.
 func submit(fields: Dictionary) -> int:
 	push_error("not implemented")
 	return -1
 
+## Finds the value of the given Node based on its type.
+## Throws an error if the type is unknown.
+## BaseButton -> button_pressed: bool
+## LineEdit | TextEdit -> text: String
+## Slider | SpinBox -> value: float
+## GraphEdit -> get_connection_list(): Array[Dictionary]
+## ItemList -> items: Array[{
+##	selected = is_selected(): bool,
+##	text: get_item_text(): String,
+##	icon: get_item_icon(): Texture,
+##	metadata: get_item_metadata(): Variant
+## }]
 func get_value(subject: Node):
 	if subject is BaseButton:
 		return subject.button_pressed
