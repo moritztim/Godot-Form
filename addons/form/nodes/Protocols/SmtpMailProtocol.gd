47bc1df9937f58c4eca2a64a24d72c24ef2af838
Moritz Tim W. <90388353+moritz-t-w@users.noreply.github.com>
2023-11-07 11:31:10 +0100

    doc & return type

--- addons/form/nodes/Protocols/SmtpMailProtocol.gd
+++ addons/form/nodes/Protocols/SmtpMailProtocol.gd
@@ -1,4 +1,5 @@
 @tool
+## Handles form submission and response over the network using Simple Mail Transfer Protocol (SMTP).
 class_name SmtpMailProtocol extends MailProtocol
 
 enum SecurityType {
@@ -6,24 +7,39 @@ enum SecurityType {
 	SSL = 465,
 	TLS = 587
 }
-@export var securityType: SecurityType = SecurityType.SSL:
+## Security Type
+## Adjusts the port if it is 0, 465 or 587.
+@export var security_type: SecurityType = SecurityType.SSL:
 	set(new_val):
-		securityType = new_val
+		security_type = new_val
 		if port in SecurityType.values():
 			port = new_val # TODO: this does not work and I have no idea why
 
 @export_group("Authentication")
-@export var useAuthentication := true
+## Authenticate with the mail server
+@export var use_authentication := true
+## Username at the mail server
 @export var username: String
+## Password at the mail server
 @export var password: String
 
-func _init():
+## Sets the port based on the security type if it is -1.
+func _init() -> void:
 	if port == -1:
-		port = securityType
+		port = security_type
 
-func submit(fields:Dictionary) -> int:
+## Returns handle_smtp(generate_body(fields)).
+func submit(
+	## Output of FormContainer.generate_fields_dict() to populate body
+	fields:Dictionary
+) -> int:
 	return handle_smtp(generate_body(fields))
 
-func handle_smtp(body: String) -> int:
+
+## Handles the SMTP request and returns the status code.
+func handle_smtp(
+	##E-Mail body
+	body: String
+) -> int:
 	push_error("not implemented")
 	return -1
