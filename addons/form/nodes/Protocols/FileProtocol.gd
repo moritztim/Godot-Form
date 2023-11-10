bb378bf1ab2d3b3b8ba6366247dd307d48ab17c0
Moritz Tim W. <90388353+moritz-t-w@users.noreply.github.com>
2023-11-10 21:32:14 +0100

    Create (unfinished) FileProtocol

---
+++ addons/form/nodes/Protocols/FileProtocol.gd
@@ -0,0 +1,51 @@
+@tool
+## Handles form submission to a JSON file.
+class_name FileProtocol extends Protocol
+
+## Directory to save responses to.
+@export_global_dir var target_dir
+
+## File name scheme.
+## Available variables:
+## - {hash}: Hash of the form data.
+## - {id}: Number of existing files in the target directory + 1.
+## - {year}: Current year.
+## - {month}: Current month.
+## - {day}: Current day.
+## - {weekday}: Current weekday.
+## - {hour}: Current hour.
+## - {minute}: Current minute.
+## - {second}: Current second.
+@export var file_name_scheme := "{id}.json"
+
+## Saves form data to a new file according to the file_name_scheme and returns 200.
+func submit(    
+	## The output of FormContainer.generate_fields_dict().
+	fields: Dictionary
+) -> int:
+	var submission_time := Time.get_date_dict_from_system()
+	submission_time.merge(Time.get_time_dict_from_system())
+	if (target_dir == "" || target_dir == null):
+		push_error("No target directory specified.")
+		return 0
+
+	if DirAccess.dir_exists_absolute(target_dir) == false:
+		push_error("Target directory does not exist.")
+		return 0
+	
+	var format_dict = {
+		"id": DirAccess.get_files_at(target_dir).size() + 1,
+		"hash": fields.hash()
+	}
+	format_dict.merge(submission_time)
+	var file_name := file_name_scheme.format(format_dict)
+
+	if FileAccess.file_exists(target_dir + "/" + file_name):
+		push_error("File already exists.")
+		return 0
+	
+	var file := FileAccess.open(target_dir + "/" + file_name, FileAccess.WRITE)
+	file.store_string(JSON.stringify(fields))
+	file.close()
+
+	return 200
--- addons/form/plugin.gd
+++ addons/form/plugin.gd
@@ -14,6 +14,7 @@ func _enter_tree():
 		["Boundaries", "Resource", preload("nodes/Boundaries.gd"), preload("icons/Boundaries.svg")],		
 		["ListFilter", "Resource", preload("nodes/ListFilter.gd"), preload("icons/ListFilter.svg")],
 		["Protocol", "Resource", preload("nodes/Protocol.gd"), preload("icons/Protocol.svg")],
+		["FileProtocol", "Resource", preload("res://addons/form/nodes/Protocols/FileProtocol.gd", preload("icons/FileProtocol.svg"))]
 		["NetworkProtocol", "Resource", preload("nodes/Protocols/NetworkProtocol.gd"), preload("icons/NetworkProtocol.svg")],
 		["HttpProtocol", "Resource", preload("nodes/Protocols/HttpProtocol.gd"), preload("icons/HttpProtocol.svg")],
 		["MailProtocol", "Resource", preload("nodes/Protocols/MailProtocol.gd"), preload("icons/MailProtocol.svg")],
