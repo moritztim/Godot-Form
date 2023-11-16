805308d1927f6441c9ac13c9a15321a55486c822
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2023-11-16 18:34:12 +0100

    Update signature

--- addons/form/nodes/Validator.gd
+++ addons/form/nodes/Validator.gd
@@ -12,7 +12,7 @@ var valid := false
 var broken_rules := {}
 
 ## Validates subject against all rules and returns validity
-func validate(subject: Object) -> bool:
+func validate(subject) -> bool:
 	if typeof(subject) != get_type():
 		push_error("Subject must be of type ", get_type(), " (see Variant.Type)")
 	broken_rules = {}
