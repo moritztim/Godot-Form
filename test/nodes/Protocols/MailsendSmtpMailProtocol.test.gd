417d53e1c6c664b7e92a9af9d7b8c24e22ffacb3
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2024-01-05 17:06:15 +0100

    print jail

--- test/nodes/Protocols/MailsendSmtpMailProtocol.test.gd
+++ test/nodes/Protocols/MailsendSmtpMailProtocol.test.gd
@@ -3,30 +3,37 @@ class_name MailsendSmtpMailProtocolTest extends ClassTest
 func run():
 	var instance = MailsendSmtpMailProtocol.new()
 	instance.sanitization = MailsendSmtpMailProtocol.Sanitization.SHELL_ESCAPE
+	var jail = []
+	var actual = instance.sanitize_shell_args([
+		MailsendSmtpMailProtocol.SHELL_BLACKLIST
+	], jail)
+	print("caught:\n", jail)
 	compare(
-		instance.sanitize_shell_args([
-			MailsendSmtpMailProtocol.SHELL_BLACKLIST
-		]),
+		actual,
 		[ "\\\"\\$\\%\\`\\!" ],
 		"sanitize shell args with SHELL_ESCAPE"
 	)
 	instance.sanitization = MailsendSmtpMailProtocol.Sanitization.SHELL_WHITELIST
+	actual = instance.sanitize_shell_args([
+		MailsendSmtpMailProtocol.SHELL_WHITELIST,
+		MailsendSmtpMailProtocol.SHELL_BLACKLIST + "}",
+		MailsendSmtpMailProtocol.SHELL_WHITELIST + "}" + MailsendSmtpMailProtocol.SHELL_BLACKLIST
+	], jail)
+	print("caught:\n", jail)
 	compare(
-		instance.sanitize_shell_args([
-			MailsendSmtpMailProtocol.SHELL_WHITELIST,
-			MailsendSmtpMailProtocol.SHELL_BLACKLIST + "}",
-			MailsendSmtpMailProtocol.SHELL_WHITELIST + "}" + MailsendSmtpMailProtocol.SHELL_BLACKLIST
-		]),
+		actual,
 		[ MailsendSmtpMailProtocol.SHELL_WHITELIST, "", MailsendSmtpMailProtocol.SHELL_WHITELIST ],
 		"sanitize shell args with SHELL_WHITELIST"
 	)
 	instance.sanitization = MailsendSmtpMailProtocol.Sanitization.SHELL_BLACKLIST
+	actual = instance.sanitize_shell_args([
+		MailsendSmtpMailProtocol.SHELL_WHITELIST,
+		MailsendSmtpMailProtocol.SHELL_BLACKLIST + "}",
+		MailsendSmtpMailProtocol.SHELL_WHITELIST + "}" + MailsendSmtpMailProtocol.SHELL_BLACKLIST
+	], jail)
+	print("caught:\n", jail)
 	compare(
-		instance.sanitize_shell_args([
-			MailsendSmtpMailProtocol.SHELL_WHITELIST,
-			MailsendSmtpMailProtocol.SHELL_BLACKLIST + "}",
-			MailsendSmtpMailProtocol.SHELL_WHITELIST + "}" + MailsendSmtpMailProtocol.SHELL_BLACKLIST
-		]),
+		actual,
 		[ MailsendSmtpMailProtocol.SHELL_WHITELIST + "}", "}", MailsendSmtpMailProtocol.SHELL_WHITELIST + "}" ],
 		"sanitize shell args with SHELL_BLACKLIST"
 	)
