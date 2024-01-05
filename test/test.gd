e4145aac2493960ace4da34fbaaa6b31645214fb
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2024-01-05 15:04:31 +0100

    improve testing

--- test/test.gd
+++ test/test.gd
@@ -1,45 +1,43 @@
 #gdscript
-class_name ClassTest extends Object
-## Run all tests
-## Returns a Dictionary of test names and results
-func run() -> Dictionary:
+class_name ClassTest extends Node
+
+## Dictionary of test names and results
+var results = {}
+
+## Print test results
+@export var verbose = true
+
+## Run all tests and update results
+func run():
 	return {}
 
 ## Call run() and print results
-func run_verbose():
-	var results = run()
+func print_report():
+	run()
 	if results.size() == 0:
-		print("❔ No tests found")
+		print("❔ No tests ran")
 		return
-	var i = 1
-	var length = str(results.size()).length()
-	for test in results:
-		# 1 /10: ✅ passed 'test 1'
-		# 10/10: ❌ failed 'test 10'
-		print(
-			pad(str(i), length, " "), "/", results.size(), ": ",
-			"✅ passed" if results[test] else "❌ failed",
-			" '", test, "'"
-		)
-		i += 1
 	print(
 		"✅" if results.values().all(func(x): return x) else "❌",
-		" ", pad(results.values().count(true), length, " "), "/", results.size(), " tests passed."
+		" ", results.values().count(true), "/", results.size(), " tests passed."
 	)
-	
 
-func pad(
-	subject,
-	## Desired length of the string
-	length,
-	## Character to pad with, if not set or not length 1, it defaults to " "
-	character = " ",
-	## Pad to the left
-	left = false
-) -> String:
-	if character.length() != 1:
-		character = " "
-	var result = str(subject)
-	while result.length() < length:
-		result = character + result if left else result + character
-	return result
+func compare(
+	## Actual value
+	actual,
+	## Expected value
+	expected,
+	## Name of test
+	name
+):
+	if verbose: print("\n", "------", name, "------")
+	results[name] = actual == expected
+	if !verbose: return
+	print(
+		results.size(), " ",
+		"✅ passed" if results[name] else "❌ failed"
+	)
+	print(
+		"\n 🟢 expected: \n", expected,
+		"\n 🔴 actual: \n", actual
+	)
