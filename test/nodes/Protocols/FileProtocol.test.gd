564ba6e944c213bcc05e40b2a0bc4fb61643e674
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2024-01-08 16:06:57 +0100

    add tests

---
+++ test/nodes/Protocols/FileProtocol.test.gd
@@ -0,0 +1,27 @@
+class_name FileProtocolTest extends ClassTest
+
+func run():
+	var instance = FileProtocol.new()
+	
+	instance.file_name_scheme = '{hash}-{id}-{year}-{month}-{day}-{weekday}-{hour}-{minute}-{second}.json'
+
+	# create temporary directory
+	instance.target_dir = '/tmp/godot-FileProtocol-test'
+	DirAccess.make_dir_absolute(instance.target_dir)
+	var i = 1
+	while (!DirAccess.dir_exists_absolute(instance.target_dir)):
+		instance.target_dir = '/tmp/godot-FileProtocol-test' + i
+		DirAccess.make_dir_absolute(instance.target_dir)
+		i += 1
+	
+	var format_dict = {
+		hash = {}.hash(),
+		id = 1
+	}
+	format_dict.merge(Time.get_date_dict_from_system())
+	format_dict.merge(Time.get_time_dict_from_system())
+	compare(
+		instance.generate_file_name({}),
+		instance.file_name_scheme.format(format_dict),
+		'generate file name'
+	)
